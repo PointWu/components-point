462e328c9d478abb0198f571e08a4803ce3dda14
Point <wuyuhuiss@outlook.com>
2023-02-03 15:28:13 +0800

    feat(init project): init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/point.png" />
+  <HelloWorld msg="Hello Point" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  
+  <p>📦</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+     
+    }
+  }
+}
+</script>
